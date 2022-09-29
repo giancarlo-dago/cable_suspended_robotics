f50b850f8390ddfa410ec83e32660eb6735b0f36
Giancarlo <giancarlo.dago@unina.it>
2022-09-29 16:23:18 +0200

    Code cleaning

--- src/licas/naturalResponseLicas.cpp
+++ src/licas/naturalResponseLicas.cpp
@@ -27,59 +27,49 @@ class IDENTIFICATION {
         double _shoulder_link_pose[7];
         double _gazeboTime;
         double _resetWaitTime;
-        double _startingAngle1;
-        double _startingAngle2;
-        double _startingAngle3;
-        double _startingAngle4_1;
-        double _startingAngle4_2;
-        bool _video1Flag;
-        bool _video2Flag;        
     public:
         IDENTIFICATION();
         void run();
         void readMeasures();
-        void linkStateCB(gazebo_msgs::LinkStates);
-        void gazeboClockCB(rosgraph_msgs::Clock);
+        void linkStateCB(gazebo_msgs::LinkStates);          // Link state callback
+        void gazeboClockCB(rosgraph_msgs::Clock);           // Gazebo clock callback
 };
 
 
+// Constructor
 IDENTIFICATION::IDENTIFICATION() {
-    _pauseGazeboClient = _nh.serviceClient<std_srvs::Empty>("/gazebo/pause_physics");
-    _unpauseGazeboClient = _nh.serviceClient<std_srvs::Empty>("/gazebo/unpause_physics");
-    _setModelConfigurationClient = _nh.serviceClient<gazebo_msgs::SetModelConfiguration>("/gazebo/set_model_configuration");
-    _linkStateSub = _nh.subscribe("/gazebo/link_states",0,&IDENTIFICATION::linkStateCB,this);
-    _gazeboClockSub = _nh.subscribe("/clock",0,&IDENTIFICATION::gazeboClockCB,this);
+    _pauseGazeboClient = _nh.serviceClient<std_srvs::Empty>("/gazebo/pause_physics");                                               // Pause Gazebo Client
+    _unpauseGazeboClient = _nh.serviceClient<std_srvs::Empty>("/gazebo/unpause_physics");                                           // Unpause Gazebo Client
+    _setModelConfigurationClient = _nh.serviceClient<gazebo_msgs::SetModelConfiguration>("/gazebo/set_model_configuration");        // Set Model Configuration Client
+    _linkStateSub = _nh.subscribe("/gazebo/link_states",0,&IDENTIFICATION::linkStateCB,this);                                       // Link State subscriber
+    _gazeboClockSub = _nh.subscribe("/clock",0,&IDENTIFICATION::gazeboClockCB,this);                                                // Gazebo Clock Subscriber
 
     // Definition of the service request message
     _configSrv.request.model_name = "LiCAS_A1";                             // Model name
     _configSrv.request.urdf_param_name = "robot_description";               // URDF param name (unused)
-    std::vector<std::string> name_vec(6);                                   // Joint names vector
-    name_vec.push_back("revolute_joint_x");
-    name_vec.push_back("revolute_joint_y");
-    name_vec.push_back("revolute_joint_z");
-    name_vec.push_back("shoulder_joint_x");
-    name_vec.push_back("shoulder_joint_y");
-    name_vec.push_back("shoulder_joint_z");
-    _configSrv.request.joint_names = name_vec;
+
+    // Definition of the joint names and initial position vectors
+    std::vector<std::string> joints_name_vec;                               // Joint names [rad] vector
+    std::vector<double> joints_pos_vec;                                     // Joint position [rad] vector
+    joints_name_vec.insert(joints_name_vec.end(), {"revolute_joint_z","revolute_joint_x","revolute_joint_y","shoulder_joint_z","shoulder_joint_x","shoulder_joint_y"});
+    joints_pos_vec.insert(joints_pos_vec.end(), {0.5, 0, 0, 0, 0, 0});      // [rad]
+
+    // Configuration of the service
+    _configSrv.request.joint_names = joints_name_vec;                       // Configuring the server with joints name list
+    _configSrv.request.joint_positions = joints_pos_vec;
 
     // Initializations
-    _gazeboTime = 0;
+    _gazeboTime = 0;                                                        // Initializing Gazebo time variable
     _resetWaitTime = 5;                                                     // Time to wait while positioning the system to a nonrest configuration [sec]
-
-    _startingAngle1 = 0.1983;                                               // [rad]
-    _startingAngle2 = 0.2620;                                               // [rad]
-    _startingAngle3 = 0.9741;                                               // [rad]
-    _startingAngle4_1 = 0.1881;                                            // [rad]
-    _startingAngle4_2 = 0.1678;                                             // [rad]
-
 }
+
 
 // Callback for saving the pose of the links
 void IDENTIFICATION::linkStateCB(gazebo_msgs::LinkStates lstate) {
-    _shoulder_link_pose[0] = lstate.pose[6].position.x;
+    _shoulder_link_pose[0] = lstate.pose[6].position.x;                     // Position
     _shoulder_link_pose[1] = lstate.pose[6].position.y;
     _shoulder_link_pose[2] = lstate.pose[6].position.z;
-    _shoulder_link_pose[3] = lstate.pose[6].orientation.x;
+    _shoulder_link_pose[3] = lstate.pose[6].orientation.x;                  // Orientation in quaternions
     _shoulder_link_pose[4] = lstate.pose[6].orientation.y;
     _shoulder_link_pose[5] = lstate.pose[6].orientation.z;
     _shoulder_link_pose[6] = lstate.pose[6].orientation.w;
@@ -95,81 +85,45 @@ void IDENTIFICATION::gazeboClockCB(rosgraph_msgs::Clock clockMsg) {
 }
 
 
+// Modify the initial state of the system
 void IDENTIFICATION::readMeasures() {
-
-    double startTime = _gazeboTime;
-    double elapsedTime = 0; 
-    double currentTime = 0;
-    std::ofstream simdatafile;
-    std::vector<double> pos_vec(6);                                         // Joint position [rad] vector
 
     // Define Sampling rate
     ros::Rate r(50);
 
-    // --------------------- FIRST EXPERIMENT ---------------------
-        
+    // Initialization for data saving (to be inserted)
+    std::ofstream simdatafile;
+
     // Initialization for time sampling
-    startTime = _gazeboTime;
-    elapsedTime = 0; 
-    currentTime = 0;
+    double startTime = _gazeboTime;
+    double elapsedTime = 0; 
+    double currentTime = 0;
+    double oldElapsed = 0;
 
-    // Define the desired starting joint position
-    // pos_vec.push_back(_startingAngle1);
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(0.0);
-
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(_startingAngle2);
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(0.0);
-
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(0.0);
-    // pos_vec.push_back(_startingAngle3);
-    // pos_vec.push_back(0.0);
-
-    pos_vec.push_back(_startingAngle4_2);
-    pos_vec.push_back(_startingAngle4_1);
-    pos_vec.push_back(0.0);
-    pos_vec.push_back(-_startingAngle4_2);
-    pos_vec.push_back(-_startingAngle4_1);
-    pos_vec.push_back(0.0);
-
-    _configSrv.request.joint_positions = pos_vec;
-
-    // Move the robot to a non-rest initial configuration by calling the gazebo/setModelConfiguration service
-    while (elapsedTime<=_resetWaitTime) {
-        currentTime = _gazeboTime; 
-        elapsedTime = currentTime - startTime;
-        _setModelConfigurationClient.call(_configSrv);
-        std::cout << elapsedTime<< std::endl;
-        r.sleep();
-    }
-    
-    double oldElapsed = elapsedTime;
-
-    while (elapsedTime<=oldElapsed+_resetWaitTime) {
-        currentTime = _gazeboTime;
-        elapsedTime = currentTime - startTime;
-        _setModelConfigurationClient.call(_configSrv);
-        std::cout << elapsedTime<< std::endl;
-        r.sleep();
+    // Move the robot to a non-rest initial configuration by calling the gazebo/setModelConfiguration service and waiting _resetWaitTime seconds
+    for (int i=0; i<2; i++) {                                       // In the first loop the value of elapsed time is wrong (not sure why) so two loops are needed
+        while (elapsedTime<=oldElapsed+_resetWaitTime) {
+            currentTime = _gazeboTime; 
+            elapsedTime = currentTime - startTime;
+            _setModelConfigurationClient.call(_configSrv);          // Set model initial state
+            r.sleep();
+        }
+        oldElapsed = elapsedTime;    
     }
     std::cout << "Server says [ " << _configSrv.response.status_message << " ] " << std::endl;
 
     ros::shutdown();
-
-
 }
 
 
+// Run loop
 void IDENTIFICATION::run() {
     boost::thread readMeasures_t(&IDENTIFICATION::readMeasures,this);
     ros::spin();
 }
 
 
+// Main loop
 int main (int argc, char** argv) {
     ros::init(argc, argv, "identification");
     IDENTIFICATION identification;
